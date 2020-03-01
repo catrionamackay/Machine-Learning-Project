43bd56fca3b73f394f9090345deaf9d58db95fc7
catrionamackay <catriona1296@gmail.com>
2020-03-01 11:38:00 +0000

    Tidying

--- Imputation_script.py
+++ Imputation_script.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.experimental import enable_iterative_imputer 
 from sklearn.impute import IterativeImputer
-from sklearn.impute import KNNImputer
-import seaborn as sns
 
 
-df = pd.read_csv('Processed_data_full.csv', index_col=[0])
+df = pd.read_csv('Data/Processed_data_full.csv', index_col=[0], low_memory=False)
 
 df = df.drop(['marital_stat'], axis=1)
 
@@ -30,28 +27,3 @@ df_cat = df_cat.drop(['birthweight_g', 'birthweight_bin'], axis=1)
 df_imputed.to_csv('Data/MICE_data.csv')
 df_bin.to_csv('Data/MICE_data_bin.csv')
 df_cat.to_csv('Data/MICE_data_cat.csv')
-
-
-
-df_KNN = df.copy()
-
-KNN_imputer = KNNImputer(n_neighbors=5)
-df_KNN = KNN_imputer.fit_transform(df)
-df_KNN = pd.DataFrame(df_KNN)
-
-df_colnames = df.columns
-df_KNN.columns = df_colnames
-
-df_knn_bin = df_KNN.copy()
-df_knn_bin['birthweight_bin'] = np.where(df_knn_bin['birthweight_g'] < 2500, 1, 0)
-
-df_knn_cat = df_knn_bin.copy()
-df_knn_cat['birthweight_cat'] = np.where(df_knn_cat['birthweight_g'] < 1500, 2, df_knn_cat['birthweight_bin'])
-
-df_knn_bin = df_knn_bin.drop(['birthweight_g'], axis=1)
-df_knn_cat = df_knn_cat.drop(['birthweight_g', 'birthweight_bin'], axis=1)
-
-
-df_KNN.to_csv('Data/KNN_data.csv')
-df_knn_bin.to_csv('Data/KNN_data_bin.csv')
-df_knn_cat.to_csv('Data/KNN_data_cat.csv')
