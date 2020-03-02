d292536da5b85508533e8b33b417b32cd004f7f8
catrionamackay <catriona1296@gmail.com>
2020-03-02 16:52:38 +0000

    Worked on imputation

--- Imputation_script_KNN.py
+++ Imputation_script_KNN.py
@@ -11,16 +11,4 @@ df_KNN = pd.DataFrame(KNN_imputer.fit_transform(df))
 df_colnames = df.columns
 df_KNN.columns = df_colnames
 
-df_knn_bin = df_KNN.copy()
-df_knn_bin['birthweight_bin'] = np.where(df_knn_bin['birthweight_g'] < 2500, 1, 0)
-
-df_knn_cat = df_knn_bin.copy()
-df_knn_cat['birthweight_cat'] = np.where(df_knn_cat['birthweight_g'] < 1500, 2, df_knn_cat['birthweight_bin'])
-
-df_knn_bin = df_knn_bin.drop(['birthweight_g'], axis=1)
-df_knn_cat = df_knn_cat.drop(['birthweight_g', 'birthweight_bin'], axis=1)
-
-
 df_KNN.to_csv('Data/KNN_data.csv')
-df_knn_bin.to_csv('Data/KNN_data_bin.csv')
-df_knn_cat.to_csv('Data/KNN_data_cat.csv')
